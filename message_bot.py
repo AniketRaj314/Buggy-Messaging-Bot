18d1340de2ff4ebec82c521746205187165eea59
Alok Bhawankar <alok.bhawankar@gmail.com>
2019-10-02 00:56:28 +0530

    message

--- message_bot.py
+++ message_bot.py
@@ -2,16 +2,16 @@ import pyautogui as py
 import pandas as pd
 import time
 
-whatsapp_api = "https://api.whatsapp.com/send?phone=91"
+whatsapp_api = "https://api.whatsapp.com/send?phone=91"  //whatsapi
 hello = "Hey, "
 message = "This is to remind you that the C/C++ workshop is tomorrow at 3:30pm. Location will be given out soon (Most probably SL 2,3 Lab in N-building, 1st floor). Do confirm if you'll be attending the workshop :D\n\nSee you tomorrow!"
 
-def timer():
+def timer():   //setting up timer
     for x in range(5, 0, -1):
         print(x)
         time.sleep(1)
 
-def sendMessage(num, name):
+def sendMessage(num, name): //send message
     api = whatsapp_api + str(num)
     final = hello + name + "!\n" + message
     print(api, name)
@@ -30,7 +30,7 @@ def sendMessage(num, name):
 
 if __name__ == "__main__":
     timer()
-    excel = pd.read_excel("New.xlsx")
+    excel = pd.read_excel("New.xlsx")  //reading excel file
     numbers = excel['Number'].tolist()
     names = excel['Name'].tolist()
     ctr = 0
